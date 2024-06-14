0cfcb4b18f3e1232680170137b57e112f10feb9f
jacobh310 <jacob.hernandez0830@gmail.com>
2024-06-13 17:53:26 -0700

    modified data retrieval

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,17 +1,35 @@
 from sec_edgar_downloader import Downloader
 import yaml
+from bs4 import BeautifulSoup
+from utils import get_all_file_paths
 
-with open("../../configs/data.yaml") as f:
+with open("..\\..\\configs\\data.yaml") as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-path_to_save = '../../data/raw'
+path = '../../data/raw'
 EMAIL = cfg['email']
 TICKERS = cfg['tickers']
 AMOUNT = cfg['amount'] 
 
-dl = Downloader("RAG", EMAIL, path_to_save,)
+dl = Downloader("RAG", EMAIL, path,)
 
 for ticker in TICKERS:
     dl.get("10-K", ticker, limit= AMOUNT)
 
 print("Finish Downloading")
+
+directory_path = "..\..\data\\raw\sec-edgar-filings"
+file_paths = get_all_file_paths(directory_path)
+
+print("Parsing HTML and Saving")
+
+for path in file_paths:
+    
+    file = open(path, 'r', encoding='utf-8')
+    soup = BeautifulSoup(file, 'html.parser')
+    text = soup.sequence
+    file.close()
+    with open(path,"w",encoding='utf-8',errors='ignore') as file:
+        file.write(str(text))
+
+print("Finished")
